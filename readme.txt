89ef3a19543bb8b989d95ffadb7c42319d69840a
William <wgaintsev@gmail.com>
2019-05-08 22:49:02 +1000

    Created readme and js file

---
+++ readme.txt
@@ -0,0 +1,14 @@
+# Project: todoList
+This is a project to create a todoList utilizing HTML 5, CSS3 and Vanilla JS.
+
+## Approach
+
+I will be using an iterative approach to build out the core functionality of the todoList, breaking down the requirements into small manageable chunks and expanding upon them to add increased functionality.
+
+## Features
+The different features will include:
+- Adding todos
+- Changing todos
+- Deleting todos
+- Toggling one todo
+- Toggling all todos.
---
+++ todoList.js
@@ -0,0 +1,16 @@
+// Requirements 1
+
+// It should have a place to store todos
+var todos = ['item 1', 'item 2', 'item 3']              
+
+// It should have a way to display todos
+console.log('My todos:', todos);                        
+
+// It should have a way to add todos
+todos.push('new todo');                                 
+
+// It should have a way to change todos
+todos[0] = 'Changed';                                
+
+// It should have a way to delete todos
+todos.splice[0];                                        
