42685896fae48a65fef6dddac1070b5fbfeea0c4
William <wgaintsev@gmail.com>
2019-05-11 18:40:50 +1000

    Requirement 3

--- todoList.js
+++ todoList.js
@@ -1,27 +1,21 @@
-// Requirement 2
+// Requirement 3
 
-// It should have a place to store todos
-var todos = ['item 1', 'item 2', 'item 3']              
-
-// It should have a function to display todos
-function displayTodos() {
-    console.log('My todos: ', todos);
-}
-
-// It should have a function to add todos
-function addTodo(todo) {
-    todos.push(todo);
-    displayTodos();
-}
-
-// It should have a function to change todos
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos();
-}
-
-// It should have a function to delete todos
-function deleteTodo(position) {
-    todos.splice(position, 1);
-    displayTodos();
-}                                      
+// It should store the todos array on an object
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function() {                  // It should have a displayTodos method
+        console.log('My Todos: ', this.todos);
+    },
+    addTodo: function(todo) {                   // It should have an addTodos method
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {  // It should have a changeTodos method
+        this.todos[position] = nevalue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {            // It should have a deleteTodos method
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
