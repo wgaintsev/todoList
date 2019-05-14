eccef794e3f82fbcaa44ffe726fb3e2f14510836
William <wgaintsev@gmail.com>
2019-05-14 18:54:24 +1000

    Requirement 6

--- todoList.js
+++ todoList.js
@@ -1,14 +1,12 @@
-// Requirement 5
+// Requirement 6
 
 var todoList = {
     todos: [],
     displayTodos: function() {        
-        // .displayTodos should tell you if .todos is empty
         if(this.todos.length === 0) {                      
             console.log("Your todo list is empty!");                
         } else {
             console.log('My Todos:');
-            // .displayTodos should show .todoText as well as .completed
             for (var i = 0; i < this.todos.length; i++) {           
                 if(this.todos[i].completed === false) {             
                     console.log('( ) ' + this.todos[i].todoText);  
@@ -37,6 +35,25 @@ var todoList = {
     toggleCompleted: function(position) {           
         var todo = this.todos[position];
         todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;                
+        var completedTodos = 0;                             
+        for (var i = 0; i < this.todos.length; i++) {       
+            if (this.todos[i].completed === true) {        
+                completedTodos++                            
+            } 
+        }
+        if (totalTodos === completedTodos) {                // .toggleAll: If everything's true, make everything false.
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].completed = false
+            }
+        } else {                                            // .toggleAll: Otherwise, make everything true.
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].completed = true;
+            }
+        }
         this.displayTodos();
     }
 };
