2b46135cfd57cbba050544d4bb5bee447b562169
William <wgaintsev@gmail.com>
2019-05-18 15:20:27 +1000

    Requirement 10 + Refactoring

--- todoList.js
+++ todoList.js
@@ -1,41 +1,26 @@
-// Requirement 9 Refactored
+// Requirement 10 + Refactoring
 
 var todoList = {
     todos: [],
-    displayTodos: function() {        
-        if(this.todos.length === 0) {                      
-            console.log("Your todo list is empty!");                
-        } else {
-            console.log('My Todos:');
-            for (var i = 0; i < this.todos.length; i++) {           
-                if(this.todos[i].completed === false) {             
-                    console.log('( ) ' + this.todos[i].todoText);  
-                } else {
-                    console.log('(x) ' + this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function(todoText) {                   
         this.todos.push({
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
+        view.displayTodos();
     },
     changeTodo: function(position, newValue) {      
         this.todos[position].todoText = newValue;
-        this.displayTodos();
+        view.displayTodos();
     },
-    
     deleteTodo: function(position) {            
         this.todos.splice(position, 1);
-        this.displayTodos();
+        view.displayTodos();
     },
     toggleCompleted: function(position) {           
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
+        view.displayTodos();
     },
     toggleAll: function() {
         var totalTodos = this.todos.length;                
@@ -54,37 +39,46 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
-        this.displayTodos();
+        view.displayTodos();
     }
 };
 
 var handlers = {
     displayTodos: function() {
-        todoList.displayTodos();
-    },
-    toggleAll: function() {
-        todoList.toggleAll();
+        view.displayTodos();                                                    // Run new displayTodos method
     },
     // It should have working controls for .addTodo
     addTodo: function() {
         var addTodoTextInput = document.getElementById('addTodoTextInput');
         todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = "";                                            // Set value to blank after function run
+        view.displayTodos();                                                    // Run new displayTodos method
     },
     // It should have working controls for .changeTodo
     changeTodo: function() {
         var changeTodoNumberInput = document.getElementById('changeTodoNumberInput');
         var changeTodoTextInput = document.getElementById('changeTodoTextInput');
         todoList.changeTodo(changeTodoNumberInput.value, changeTodoTextInput.value);
+        changeTodoNumberInput.value = "";                                       // Set value to blank after function run
+        changeTodoTextInput.value = "";                                         // Set value to blank after function run
+        view.displayTodos();
     },
     // It should have working controls for .deleteTodo
-    deleteTodo: function() {
-        var deleteTodoNumberInput = document.getElementById('deleteTodoNumberInput');
-        todoList.deleteTodo(deleteTodoNumberInput.value);
+    deleteTodo: function(position) {
+        todoList.deleteTodo(position);
+        view.displayTodos();                                                    // Run new displayTodos method
+
     },
     // It should have working controls for .toggleCompleted
     toggleCompleted: function() {
         var toggleCompletedNumberInput = document.getElementById('toggleCompletedNumberInput');        
-        todoList.toggleCompleted(toggleCompletedNumberInput.value);
+        todoList.toggleCompleted(toggleCompletedNumberInput.valueAsNumber);
+        toggleCompletedNumberInput.value = "";                                  // Set value to blank after function run
+        view.displayTodos();                                                    // Run new displayTodos method                      
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+        view.displayTodos();                                                    // Run new displayTodos method
     }
 };
 
@@ -96,8 +90,8 @@ var view = {
         todoUl.innerHTML = "";
         for(var i = 0; i < todoList.todos.length; i++) {    
             var todoLi = document.createElement('li');          
-            var todo = todoList.todos[i];                           // Created variable to clean up code
-            var todoTextWithCompletion = ""                         // Created variable for completed marked + todoText
+            var todo = todoList.todos[i];                          
+            var todoTextWithCompletion = ""                         
             
             if(todo.completed === false) {
                 todoTextWithCompletion = "( ) " + todo.todoText;
@@ -105,8 +99,29 @@ var view = {
                 todoTextWithCompletion = "(x) " + todo.todoText;
             }
 
-            todoLi.textContent = todoTextWithCompletion             // Assigned withCompletion to textContent 
+            todoLi.textContent = todoTextWithCompletion          
             todoUl.appendChild(todoLi);                     
+            todoLi.appendChild(this.createDeleteButton());                      // There should be a delete button for each todo
+            todoLi.id = i;                                                      // Each li should have an id that has the todo position
         }
+    },
+    createDeleteButton: function() {                                
+        var deleteButton = document.createElement('button');                    // There should be a way to create delete buttons
+        deleteButton.textContent = 'Delete';                        
+        deleteButton.className = 'deleteButton';
+        return deleteButton;        
+    },
+    setUpEventListeners: function() {
+        var todoUl = document.querySelector('ul');
+    
+        todoUl.addEventListener('click', function(event){                       // Delete buttons should have access to the todo id
+            var elementClicked = event.target;                                  // Gets the element that was clicked
+            if (elementClicked.className === 'deleteButton') {                  // Checks if element clicked is deleteButton
+                handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+            }
+        });
     }
 };
+
+view.setUpEventListeners();                                                     // Run setUpEventListeners
+
